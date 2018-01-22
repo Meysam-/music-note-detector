482371181bdb9bdcbeee62871d77276cfa6f79da
Amirhossein <mirtaba@outlook.com>
2018-01-22 23:02:10 +0330

    Adding sketch and board simulation to the project

---
+++ sketch/sketch.ino
@@ -0,0 +1,16 @@
+
+const short MaxSampelingSize = 100;
+const short proccessSize = MaxSampleingSize / 2;
+
+short input[MaxSampelingSize];
+
+void setup() {
+  // put your setup code here, to run once:
+  //TODO: think through the way we want to produce outputs
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
