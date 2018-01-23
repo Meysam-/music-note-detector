5c43893199da14dddb091566e1b154c6f515e99f
Meysam Kazemi <kazemii.meysam@gmail.com>
2018-01-24 01:47:25 +0330

    freq to note converter function added

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -82,6 +82,59 @@ long autocorrelation(short j ,short t0){
   return ret;
 }
 
+char* relateNote(float frequency){
+   if(frequency >= 85 && frequency < 92.50)
+         return "F2";
+   if(frequency >= 92.5 && frequency < 98)
+         return "F#2/Gb2";
+   if(frequency >= 98 && frequency < 103.83)
+         return "G2";
+   if(frequency >= 103.83 && frequency < 110.00)
+         return "G#2/Ab2";
+   if(frequency >= 110.00 && frequency < 116.54)
+      return "A2";
+   if(frequency >= 116.54 && frequency < 123.47)
+         return "A#2/Bb2";
+   if(frequency >= 123.47 && frequency < 130.81)
+         return "B2";
+   if(frequency >= 130.81 && frequency < 138.59)
+         return "C3";
+   if(frequency >= 138.59 && frequency < 146.83)
+         return "C#3/Db3";
+   if(frequency >= 146.83 && frequency < 155.56)
+         return "D3";
+   if(frequency >= 155.56 && frequency < 164.81)
+         return "D#3/Eb3";
+   if(frequency >= 164.81 && frequency < 174.61)
+         return "E3";
+   if(frequency >= 174.61 && frequency < 185.00)
+         return "F3";
+   if(frequency >= 185.00 && frequency < 196.00)
+         return "F#3/Gb3";
+   if(frequency >= 196.00 && frequency < 207.65)
+         return "G3";
+   if(frequency >= 207.65 && frequency < 220.00)
+         return "G#3/Ab3";
+   if(frequency >= 220.00 && frequency < 233.08)
+         return "A3";
+   if(frequency >= 233.08 && frequency < 246.94)
+         return "A#3/Bb3";
+   if(frequency >= 246.94 && frequency < 261.63)
+         return "B3";
+   if(frequency >= 261.63 && frequency < 277.18)
+         return "C4";
+   if(frequency >= 277.18 && frequency < 293.63)
+         return "C#4/Db4";
+   if(frequency >= 293.63 && frequency < 311.13)
+         return "D4";
+   if(frequency >= 311.13 && frequency < 329.63)
+         return "D#4/Eb4";
+   if(frequency >= 329.63 && frequency < 349.23)
+         return "E4";
+   if(frequency >= 349.23 && frequency < 369.99)
+         return "F4";
+}
+
 void outputFunction(long T){
     float timeSt = micros1 - micros2;
     timeSt = 1 / timeSt;
@@ -90,6 +143,10 @@ void outputFunction(long T){
     answer = 1 / answer;
     answer *= timeSt;
 
-    Serial.println(answer);
+    Serial.print(answer);
+    Serial.print(" -> ");
+    Serial.println(relateNote(answer));
 }
+
+
 
